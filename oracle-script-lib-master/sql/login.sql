5876df0291dafcd828055114a5a808fe97fdf723
Amit <97467310+Amit-2022@users.noreply.github.com>
2023-02-26 14:13:30 +0530

    Add files via upload

---
+++ oracle-script-lib-master/sql/login.sql
@@ -0,0 +1,18 @@
+
+set serveroutput on format wrapped size unlimited
+
+set sqlprompt "_USER'@'_CONNECT_IDENTIFIER _PRIVILEGE> "
+
+define _editor=vi
+
+set tab off
+
+set pagesize 100
+set linesize 200 trimspool off
+set verify off
+set pause off
+set feed on
+
+
+
+
